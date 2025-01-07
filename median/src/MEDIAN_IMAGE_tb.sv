3c441133184654c8e34370417cb3ee831171a98b
JuanNaranjo17 <jnaranjoj@unal.edu.co>
2025-01-07 02:25:51 +0100

    MEDIAN

--- median/src/MEDIAN_IMAGE_tb.sv
+++ median/src/MEDIAN_IMAGE_tb.sv
@@ -1,71 +1,92 @@
-/* L'environnement de simulation de MEDIAN est plus simple que celui de
-   MED car nous n'avons pas generer le signal de controle BYP et car
-   nous savons quand la sortie de MEDIAN est valide grace au signal DSO.
-   Le reste est tres similaire ... */
+/* The simulation environment of MEDIAN is simpler than that of
+   MED because we do not have to generate the BYP control signal and because
+   we know when the MEDIAN output is valid thanks to the DSO signal.
+   The rest is very similar ... */
 
-`timescale 1ns/10ps
+`timescale 1ps/1ps
 
 module MEDIAN_IMAGE_tb;
 
-  logic [7:0] DI;
-  logic CLK, nRST, DSI;
+  bit [7:0] DI;
+  bit CLK, nRST, DSI;
   wire [7:0] DO;
   wire DSO;
 
   MEDIAN I_MEDIAN(.DI(DI), .DSI(DSI), .nRST(nRST), .CLK(CLK), .DO(DO), .DSO(DSO));
 
+// Function calculating the median value by performing a bubble sort.
+// It will be used as a reference.
+  function int unsigned med_ref (input int unsigned V [0:8]);
+    int unsigned tmp;
+    for(int j = 0; j < 8; j = j + 1)
+      for(int k = j + 1; k < 9; k = k + 1)
+        if(V[j] < V[k]) begin
+          tmp = V[j];
+          V[j] = V[k];
+          V[k] = tmp;
+        end
+        return V[4];
+  endfunction
+
+
   always #10ns CLK = ~CLK;
 
   initial begin: ENTREES
 
-    integer x, y, rx, ry, i, j, v[8+1], tmp, of;
-    logic [7:0] img[256*256];
+    int unsigned V[0:8];
+    int of;
+    logic [7:0] img[0:256*256-1];
 
+    // Image result in pgm format
     of = $fopen("bogart_filtre.pgm");
     $fdisplay(of, "P2 256 256 255");
+
+    // image source
     $readmemh("bogart_bruite.hex", img);
-    CLK = 1'b0;
-    DSI = 1'b0;
-    nRST = 1'b0;
+
     @(negedge CLK);
     nRST = 1'b1;
-    for(y = 0; y < 256; y = y + 1)
-      for(x = 0; x < 256; x = x + 1) begin
-        for(i = - 1; i < 2; i = i + 1)
-          for(j = - 1; j < 2; j = j + 1) begin
+
+    for(int y = 0; y < 256; y = y + 1)
+      for(int x = 0; x < 256; x = x + 1) begin
+        for(int i = - 1; i < 2; i = i + 1)
+          for(int j = - 1; j < 2; j = j + 1) begin
+            int rx,ry;
             rx = x + j;
             ry = y + i;
             rx = (rx == -1) ? 0 : rx;
             rx = (rx == 256) ? 255 : rx;
             ry = (ry == -1) ? 0 : ry;
             ry = (ry == 256) ? 255 : ry;
-            v[3 * (i + 1) + j + 1] = img[256 * ry + rx];
+            V[3 * (i + 1) + j + 1] = img[256 * ry + rx];
           end
+
         @(negedge CLK);
         DSI = 1'b1;
-        for(i = 0; i < 9; i = i + 1) begin
-          DI = v[i];
+        for(int i = 0; i < 9; i = i + 1) begin
+          DI = V[i];
           @(negedge CLK);
         end
         DSI = 1'b0;
+
         while(DSO == 1'b0)
           @(posedge CLK);
-        for(i = 0; i < 8; i = i + 1)
-          for(j = i + 1; j < 9; j = j + 1)
-            if(v[i] < v[j]) begin
-              tmp = v[i];
-              v[i] = v[j];
-              v[j] = tmp;
-            end
-        if(DO !== v[4]) begin
-          $display("erreur : DO = ", DO, " au lieu de ", v[4]);
-          $stop;
+
+        if(DO !== med_ref(V)) begin
+          $display("************************************");
+          $error("Error : DO = %0d instead of %0d", DO,V[4]);
+          $display("************************************");
+          $stop();
         end
         $fdisplay(of, "%d", DO);
       end
+
+    $display("************************************");
+    $display("End of simulation without any errors");
+    $display("************************************");
+
     $fclose(of);
-    $display("Fin de simulation sans aucune erreur");
-    $finish;
+    $finish();
   end
 
 endmodule
--- median/src/MEDIAN_tb.sv
+++ median/src/MEDIAN_tb.sv
@@ -3,59 +3,101 @@
    we know when the MEDIAN output is valid thanks to the DSO signal.
    The rest is very similar ... */
 
-`timescale 1ns/10ps
+
+`timescale 1ps/1ps
 
 module MEDIAN_tb;
 
-  logic [7:0] DI;
-  logic CLK, nRST, DSI;
+  bit   [7:0] DI;
+  bit   CLK, nRST, DSI;
   wire  [7:0] DO;
   wire  DSO;
 
-  MEDIAN I_MEDIAN(  .DI(DI),.DO(DO),
-                    .CLK(CLK), .DSI(DSI),
-                    .nRST(nRST), .DSO(DSO)
-                 );
+  MEDIAN I_MEDIAN(
+    .DI   ( DI   ) , .DO  ( DO  ) ,
+    .CLK  ( CLK  ) , .DSI ( DSI ) ,
+    .nRST ( nRST ) , .DSO ( DSO )
+  );
+
+// Function calculating the median value by performing a bubble sort.
+// It will be used as a reference.
+  function int unsigned med_ref (input int unsigned V [0:8]);
+    int unsigned tmp;
+    for(int j = 0; j < 8; j = j + 1)
+      for(int k = j + 1; k < 9; k = k + 1)
+        if(V[j] < V[k]) begin
+          tmp = V[j];
+          V[j] = V[k];
+          V[k] = tmp;
+        end
+        return V[4];
+  endfunction
 
   always #10ns CLK = ~CLK;
 
-  initial begin: INPUTS
+  initial begin: ENTREES
 
-    int i, j, k, v[8+1], tmp;
+    int unsigned V[0:8];
 
-    CLK  = 1'b0;
-    DSI  = 1'b0;
-    nRST = 1'b0;
-    @(negedge CLK);
-    nRST = 1'b1;
-    repeat(1000) begin
+    repeat(2)
+    begin
       @(negedge CLK);
-      DSI = 1'b1;
-      for(j = 0; j < 9; j = j + 1) begin
-        v[j] = {$urandom} % 256 ;
-        DI   = v[j];
+      nRST = 1'b1;
+      repeat(1000) begin
         @(negedge CLK);
+        DSI = 1'b1;
+        DI = 33;
+        repeat($random%2) @(negedge CLK);
+        for(int j = 0; j < 9; j = j + 1) begin
+          V[j] = {$random} % 256 ;
+          DI   = V[j];
+          @(negedge CLK);
+        end
+        DSI = 1'b0;
+        // Wait for DSO
+        while(DSO == 1'b0) @(posedge CLK);
+
+        if(DO !== med_ref(V)) begin
+          $display("************************************");
+          $error("Error : DO = %0d instead of %0d", DO, med_ref(V));
+          $display("************************************");
+          $stop();
+        end
+        @(posedge CLK) ;
+        if(DSO) begin
+          $display("************************************");
+          $error("Error : DSO has to pass again to 0");
+          $display("************************************");
+          $stop();
+        end
+        repeat($random%4) @(negedge CLK);
       end
-      DSI = 1'b0;
-      forever
-      begin
-        @(posedge CLK);
-           if (DSO == 1'b1) break;
-      end
-      for(j = 0; j < 8; j = j + 1)
-        for(k = j + 1; k < 9; k = k + 1)
-          if(v[j] < v[k]) begin
-            tmp = v[j];
-            v[j] = v[k];
-            v[k] = tmp;
-          end
-      if(DO !== v[4]) begin
-        $display("Error : DO = ", DO, " instead of ", v[4]);
-        $stop;
-      end
+      repeat(2) @(negedge CLK);
+      nRST = 1'b0;
+      repeat(2) @(negedge CLK);
     end
-    $display("Simulation completed successfully without any errors.");
-    $finish;
+
+    $display("************************************");
+    $display("End of simulation without any errors");
+    $display("************************************");
+    $finish();
   end
+
+  // SVA
+  property DSO_DURATION;
+    disable iff(!nRST)
+    @(posedge CLK)
+      DSO |=> !DSO;
+  endproperty
+
+  dso_duration: assert property(DSO_DURATION) else $fatal("DSO took the value 1 by more than a clock cycle");
+
+  property DSO_ONCE;
+    disable iff(!nRST)
+    @(posedge CLK)
+      $fell(DSO) |=> (!DSO until $fell(DSI));
+  endproperty
+
+  dso_once: assert property(DSO_ONCE) else $fatal("DSO has to keep 1 before the next compute");
 
 endmodule
