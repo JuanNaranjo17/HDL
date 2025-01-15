2a47bf1555cb30b782ceefbbc47233679f53641f
JuanNaranjo17 <jnaranjoj@unal.edu.co>
2025-01-15 23:21:14 +0100

    avalon_single_bram

--- controleur_memoire/avalon_bram/avalon_bram.sv
+++ controleur_memoire/avalon_bram/avalon_bram.sv
@@ -9,20 +9,23 @@
 // (voir doc mnl_avalon_spec.pdf, page 17)
 `timescale 1ns/1ps
 `default_nettype none
-module avalon_bram #(parameter integer RAM_ADD_W = 8, integer BURSTCOUNT_W = 4 ) (
+module avalon_bram #(parameter integer RAM_ADD_W = 11, integer BURSTCOUNT_W = 4 ) (
       // Avalon  interface for an agent
       avalon_if.agent avalon_a
       );
-      // a vous de jouer a partir d'ici
 
       // Local Parameters
       localparam integer DATA_WIDTH = 8 * 4; // Data width: 4 bytes = 32 bits
 
-      // Registros internos
-      logic [7:0] memory_byte0 [2**RAM_ADD_W];
-      logic [7:0] memory_byte1 [2**RAM_ADD_W];
-      logic [7:0] memory_byte2 [2**RAM_ADD_W];
-      logic [7:0] memory_byte3 [2**RAM_ADD_W];
+      // Intern Registers
+      logic [7:0] memory_byte0 [2*4*2**RAM_ADD_W];      // 2**11 = 2048
+      logic [7:0] memory_byte1 [2*4*2**RAM_ADD_W];
+      logic [7:0] memory_byte2 [2*4*2**RAM_ADD_W];
+      logic [7:0] memory_byte3 [2*4*2**RAM_ADD_W];
+      //logic [7:0] memory_byte0 [2**RAM_ADD_W];
+      //logic [7:0] memory_byte1 [2**RAM_ADD_W];
+      //logic [7:0] memory_byte2 [2**RAM_ADD_W];
+      //logic [7:0] memory_byte3 [2**RAM_ADD_W];
       logic [DATA_WIDTH-1:0] read_data_reg;  // Register to save the read data
       logic readdatavalid_reg;               // Register to readdatavalid signal
       logic waitrequest_reg;                 // Register to waitrequest signal
@@ -33,59 +36,70 @@ module avalon_bram #(parameter integer RAM_ADD_W = 8, integer BURSTCOUNT_W = 4 )
       assign avalon_a.waitrequest = waitrequest_reg;
 
       // Process as a state machine
-      typedef enum logic [1:0] {
+      typedef enum logic [1:0]{
+            RESET,
             INIT,          // Initialization
-            IDLE,          // No active state
-            READ_VALID     // Reading done
+            READ           // Reading done
       } read_state_t;
 
       read_state_t read_state;
 
+      // State machine, transition states
       always_ff @(posedge avalon_a.clk or posedge avalon_a.reset) begin
             if (avalon_a.reset) begin
-                  read_state <= INIT;
-                  read_data_reg <= 1'b0;
-                  readdatavalid_reg <= 1'b0;
                   waitrequest_reg <= 1'b1;
-            end else begin
-                  // Initialization state, only for init
+                  readdatavalid_reg <= 1'b0;
+                  read_state <= RESET;
+            end
+            else begin
                   case (read_state)
-                        INIT: begin
-                              read_data_reg <= 1'b0;
-                              readdatavalid_reg <= 1'b0;
+                        RESET: begin
                               waitrequest_reg <= 1'b0;
-                              read_state <= IDLE;
+                              readdatavalid_reg <= 1'b0;
+                              read_state <= INIT;
                         end
-                        IDLE: begin
-                              if (avalon_a.read) begin
-                                    waitrequest_reg <= 1'b1;      // Turn on waiting signal
+                        INIT: begin
+                              if(avalon_a.read == 1) begin
+                                    waitrequest_reg <= 1'b1;
                                     readdatavalid_reg <= 1'b1;
-                                    read_data_reg <= {memory_byte3[avalon_a.address],
-                                                      memory_byte2[avalon_a.address],
-                                                      memory_byte1[avalon_a.address],
-                                                      memory_byte0[avalon_a.address]};
-                                    read_state <= READ_VALID;
-                              end else if (avalon_a.write) begin
-                                    if (avalon_a.address < (2**RAM_ADD_W)) begin
-                                          // Writing without blocking the cycle
-                                          if (avalon_a.byteenable[0]) memory_byte0[avalon_a.address] <= avalon_a.writedata[7:0];
-                                          if (avalon_a.byteenable[1]) memory_byte1[avalon_a.address] <= avalon_a.writedata[15:8];
-                                          if (avalon_a.byteenable[2]) memory_byte2[avalon_a.address] <= avalon_a.writedata[23:16];
-                                          if (avalon_a.byteenable[3]) memory_byte3[avalon_a.address] <= avalon_a.writedata[31:24];
-                                    end
-                                    read_state <= IDLE;
+                                    read_state <= READ;
+                              end
+                              else begin
+                                    waitrequest_reg <= 1'b0;
+                                    readdatavalid_reg <= 1'b0;
+                                    read_state <= INIT;
                               end
                         end
 
-                        READ_VALID: begin
-                              readdatavalid_reg <= 1'b0; // Up just one cycle
+                        READ: begin
                               waitrequest_reg <= 1'b0;
-                              read_state <= IDLE;        // Back to no active
+                              readdatavalid_reg <= 1'b0;
+                              read_state <= INIT;
                         end
 
-                        default: read_state <= INIT;
+                        default: begin
+                              waitrequest_reg <= 1'b0;
+                              readdatavalid_reg <= 1'b0;
+                              read_state <= INIT;
+                        end
                   endcase
             end
       end
+      // RAM handler
+      always_ff @(posedge avalon_a.clk) begin
+            if (avalon_a.write) begin
+                  if (avalon_a.byteenable[0]) memory_byte0[avalon_a.address/4] <= avalon_a.writedata[7:0];
+                  if (avalon_a.byteenable[1]) memory_byte1[avalon_a.address/4] <= avalon_a.writedata[15:8];
+                  if (avalon_a.byteenable[2]) memory_byte2[avalon_a.address/4] <= avalon_a.writedata[23:16];
+                  if (avalon_a.byteenable[3]) memory_byte3[avalon_a.address/4] <= avalon_a.writedata[31:24];
+            end
+            else if (avalon_a.read) begin
+                  read_data_reg <= {memory_byte3[avalon_a.address/4],
+                                    memory_byte2[avalon_a.address/4],
+                                    memory_byte1[avalon_a.address/4],
+                                    memory_byte0[avalon_a.address/4]};
+            end
+      end
+
 
 endmodule
